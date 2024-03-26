e91395c07e87fca07199e4050715ffd4eac4a64b
wali-eldin-hassan <creaspo6@gmail.com>
2024-03-26 21:18:08 +0300

    inti commit

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {TypeOrmModule} from "@nestjs/typeorm";
+
+@Module({
+    imports: [TypeOrmModule.forRoot({
+        type: 'mysql',
+        host: 'localhost',
+        port: 3306,
+        database: 'demo',
+        username: 'root',
+        password: 'password',
+        synchronize: true,
+    }), ],
+    controllers: [AppController],
+    providers: [AppService],
+})
+export class AppModule {
+}
