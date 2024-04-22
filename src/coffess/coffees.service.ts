918314514c6902a90b3e5fca0054b4e45fc4829e
wali-eldin-hassan <creaspo6@gmail.com>
2024-04-22 22:44:42 +0300

    added relation and attach it to flavores when create and update

--- src/coffess/coffees.service.ts
+++ src/coffess/coffees.service.ts
@@ -1,47 +1,54 @@
-import { Injectable, NotFoundException} from '@nestjs/common';
-import {Coffee} from "./model/Coffee.entity";
-import {InjectRepository} from "@nestjs/typeorm";
-import {Repository} from "typeorm";
-import {CreateCoffeeDto} from "./dto/create-coffee.dto";
-import {UpdateCoffeeDto} from "./dto/update-coffee.dto";
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Coffee } from "./model/Coffee.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
+import { CreateCoffeeDto } from "./dto/create-coffee.dto";
+import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
+import { Flavor } from './model/Flavor.entity';
 
 @Injectable()
 export class CoffeesService {
 
-    constructor(@InjectRepository(Coffee)
-                private readonly coffeeRepository: Repository<Coffee>) {
-    }
+    constructor(
+        @InjectRepository(Coffee) private readonly coffeeRepository: Repository<Coffee>,
+        @InjectRepository(Flavor) private readonly flavorRepository: Repository<Flavor>
+    ) { }
 
     findAll() {
-        return this.coffeeRepository.find();
+        return this.coffeeRepository.find({ relations: ['flavors'] });
     }
 
     async findOne(id: number) {
 
-        const coffee = await this.coffeeRepository.findOne({ where: { id } });
+        const coffee = await this.coffeeRepository.findOne({ where: { id }, relations: ['flavors'] });
 
-        if (!coffee) {
-            throw new NotFoundException("coffee not found");
-        }
+        if (!coffee) throw new NotFoundException("coffee not found");
+
         return coffee;
     }
 
-    create(createCoffeeDto: CreateCoffeeDto) {
-        const coffee = this.coffeeRepository.create(createCoffeeDto);
+    async create(createCoffeeDto: CreateCoffeeDto) {
+
+        const flavors = await Promise.all(
+            createCoffeeDto.flavors.map(name => this.PreloadFlavoersByName(name))
+        );
+
+        const coffee = this.coffeeRepository.create({ ...createCoffeeDto, flavors });
 
         return this.coffeeRepository.save(coffee);
     }
 
     async update(id: number, updateCoffeeDto: UpdateCoffeeDto) {
-        const existing = await this.coffeeRepository.preload({
-            id: +id,
-            ...updateCoffeeDto
-        });
 
-        if (!existing) {
-            throw new NotFoundException("coffee not found")
-        }
-        return this.coffeeRepository.save(existing);
+        const flavors = updateCoffeeDto.flavors && (await Promise.all(
+            updateCoffeeDto.flavors.map(name => this.PreloadFlavoersByName(name)),
+        ));
+
+        const coffee = await this.coffeeRepository.preload({ id: +id, ...updateCoffeeDto, flavors });
+
+        if (!coffee) throw new NotFoundException("coffee not found")
+
+        return this.coffeeRepository.save(coffee);
     }
 
 
@@ -50,5 +57,14 @@ export class CoffeesService {
         const existing = await this.coffeeRepository.findOne({ where: { id } });
 
         return this.coffeeRepository.remove(existing);
+    }
+
+    private async PreloadFlavoersByName(name: string): Promise<Flavor> {
+
+        const existingFlavor = await this.flavorRepository.findOneBy({ name });
+
+        if (existingFlavor) return existingFlavor;
+
+        return this.flavorRepository.create({ name });
     }
 }
--- src/coffess/model/Coffee.entity.ts
+++ src/coffess/model/Coffee.entity.ts
@@ -15,7 +15,10 @@ export class Coffee {
     @JoinTable()
     @ManyToMany(
         type => Flavor,
-        flavor => flavor.coffees
+        flavor => flavor.coffees,
+        {
+            cascade: true,
+        }
     )
-    flavors: string[];
+    flavors: Flavor[];
 }
