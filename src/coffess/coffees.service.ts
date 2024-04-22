4bfa79cf16cca1bf6ef856d504e71d75e0a3767e
wali-eldin-hassan <creaspo6@gmail.com>
2024-04-22 22:56:42 +0300

    added paginations for query

--- src/coffess/coffees.service.ts
+++ src/coffess/coffees.service.ts
@@ -5,6 +5,7 @@ import { Repository } from "typeorm";
 import { CreateCoffeeDto } from "./dto/create-coffee.dto";
 import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
 import { Flavor } from './model/Flavor.entity';
+import { PaginationQueryDto } from 'src/common/dto/pagination-query.dto/pagination-query.dto';
 
 @Injectable()
 export class CoffeesService {
@@ -14,8 +15,13 @@ export class CoffeesService {
         @InjectRepository(Flavor) private readonly flavorRepository: Repository<Flavor>
     ) { }
 
-    findAll() {
-        return this.coffeeRepository.find({ relations: ['flavors'] });
+    findAll(paginateQurey: PaginationQueryDto) {
+        const { limit, offset } = paginateQurey;
+        return this.coffeeRepository.find({
+            relations: ['flavors'],
+            skip: offset,
+            take: limit
+        });
     }
 
     async findOne(id: number) {
