42307845044a20c2251625ecaa8904f8dfe64437
wali-eldin-hassan <creaspo6@gmail.com>
2024-04-24 08:15:32 +0300

    install joi package to validate .env vars and make sure it's imported

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,24 +1,30 @@
-import {Module} from '@nestjs/common';
-import {AppController} from './app.controller';
-import {AppService} from './app.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {CoffeesModule} from './coffess/coffees.module';
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { CoffeesModule } from './coffess/coffees.module';
 import { ConfigModule } from '@nestjs/config';
+import * as joi from '@hapi/joi';
 
 @Module({
     imports: [
-        ConfigModule.forRoot(),
+        ConfigModule.forRoot({
+            validationSchema: joi.object({
+                DATABASE_HOST: joi.required(),
+                DATABASE_PORT : joi.number().default(5432),
+            }),
+        }),
         TypeOrmModule.forRoot({
-        type: 'postgres',
-        host: process.env.DATABASE_HOST,
-        port: +process.env.DATABASE_PORT,
-        database: process.env.DATABASE_NAME,
-        username: process.env.DATABASE_USER,
-        password: process.env.DATABASE_PASSWORD,
-        synchronize: true,
-        autoLoadEntities:true,
-    }),
-     CoffeesModule,
+            type: 'postgres',
+            host: process.env.DATABASE_HOST,
+            port: +process.env.DATABASE_PORT,
+            database: process.env.DATABASE_NAME,
+            username: process.env.DATABASE_USER,
+            password: process.env.DATABASE_PASSWORD,
+            synchronize: true,
+            autoLoadEntities: true,
+        }),
+        CoffeesModule,
     ],
     controllers: [AppController],
     providers: [AppService],
