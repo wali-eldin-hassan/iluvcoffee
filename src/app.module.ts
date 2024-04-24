099e734a2216cd87af3a89da775a1eaa0ab2428b
wali-eldin-hassan <creaspo6@gmail.com>
2024-04-24 08:04:04 +0300

    added config package + move cofig attributes to .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,18 +3,23 @@ import {AppController} from './app.controller';
 import {AppService} from './app.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {CoffeesModule} from './coffess/coffees.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-    imports: [TypeOrmModule.forRoot({
+    imports: [
+        ConfigModule.forRoot(),
+        TypeOrmModule.forRoot({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        database: 'postgres',
-        username: 'postgres',
-        password: 'pass123',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        database: process.env.DATABASE_NAME,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
         synchronize: true,
         autoLoadEntities:true,
-    }), CoffeesModule,],
+    }),
+     CoffeesModule,
+    ],
     controllers: [AppController],
     providers: [AppService],
 })
